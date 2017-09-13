1ceb5fce9f2152d736f592f7e1a8d315a39c162f
Stef Sijben <git@stefsybo.nl>
2017-09-13 14:09:22 +0200

    Widget definition file

---
+++ spot-widget.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Spot Widget
+Plugin URI: http://blog.stfs.eu/
+Description: A widget showing Spot GPS tracker locations
+Version: 0.1
+Author: Stef Sijben
+Author URI: http://stfs.eu/
+License: GPL3
+*/
+
