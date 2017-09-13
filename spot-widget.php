0b09b8001933cbfbea140de27ef2471dbadcd40c
Stef Sijben <git@stefsybo.nl>
2017-09-13 14:12:24 +0200

    Register widget with Wordpress

--- spot-widget.php
+++ spot-widget.php
@@ -9,3 +9,28 @@ Author URI: http://stfs.eu/
 License: GPL3
 */
 
+class wp_spot_widget extends WP_Widget {
+
+	// constructor
+	function wp_spot_widget() {
+        parent::WP_Widget(false, $name = __('Spot GPS locations', 'wp_spot_widget') );
+	}
+
+	// widget form creation
+	function form($instance) {
+	/* ... */
+	}
+
+	// widget update
+	function update($new_instance, $old_instance) {
+		/* ... */
+	}
+
+	// widget display
+	function widget($args, $instance) {
+		/* ... */
+	}
+}
+
+// register widget
+add_action('widgets_init', create_function('', 'return register_widget("wp_spot_widget");'));
